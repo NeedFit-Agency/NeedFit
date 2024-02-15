2fe84a99fa26e8a310bbacab9fdc3bf0fedcd8f5
vinuu <vinaykhanagavi502@gmail.com>
2024-02-15 13:24:32 +0530

    Changes to be committed: 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,4 +60,16 @@ const headerActive = function () {
   }
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.querySelectorAll('.filter-btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      buttons.forEach(btn => btn.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
+
+
 addEventOnElem(window, "scroll", headerActive);
